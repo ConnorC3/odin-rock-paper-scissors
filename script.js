0484c4e15f501ce8f694ce61e9da55ccf6ae2c83
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-02-17 03:34:05 -0500

    Add play round and play game functionality
    Also added some basic input validation, still
    working on validating cancelling though

--- script.js
+++ script.js
@@ -14,13 +14,50 @@ function getComputerChoice(){
 function getHumanChoice(){
     choice = prompt("Type rock, paper, or scissors");
 
+    if (choice === null) { // if human clicks cancel on prompt
+        console.log("Player cancelled");
+        return;
+    }
+
     choice = choice.toLowerCase()
 
     if (choice !== "rock" && choice !== "paper" && choice !== "scissors"){
         console.log("Invalid Input");
     } else {
-        return choice;
+        return choice.charAt(0).toUpperCase() + choice.slice(1);
     }
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Tie game!");
+    } else if (
+        (humanChoice === "Paper" && computerChoice === "Rock") ||
+        (humanChoice === "Scissors" && computerChoice === "Paper") ||
+        (humanChoice === "Rock" && computerChoice === "Scissors")
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
+
+function playGame(){
+    for (i = 0; i < 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        if (humanSelection !== undefined && humanSelection !== null){
+            playRound(humanSelection, computerSelection);
+        }
+    }
+
+    humanScore > computerScore ? console.log("You win!") : console.log("You lose!");
+}
+
+playGame();
