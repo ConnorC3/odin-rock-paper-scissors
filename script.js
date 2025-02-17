2c2594b191d723b54db0444318fd20b9c4214fdd
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-02-17 03:09:47 -0500

    Add computer and human choice functions

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+function getComputerChoice(){
+    choice = Math.floor(Math.random()*3);
+
+    switch (choice){
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
+
+function getHumanChoice(){
+    choice = prompt("Type rock, paper, or scissors");
+
+    choice = choice.toLowerCase()
+
+    if (choice !== "rock" && choice !== "paper" && choice !== "scissors"){
+        console.log("Invalid Input");
+    } else {
+        return choice;
+    }
+}
+
+console.log(getHumanChoice());
