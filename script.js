f1b90987fb08372120ad5b4a3731662a466a99ad
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-02-18 20:42:31 -0500

    Add initial UI

--- script.js
+++ script.js
@@ -11,23 +11,39 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    choice = prompt("Type rock, paper, or scissors");
+// function getHumanChoice(){
+//     choice = prompt("Type rock, paper, or scissors");
 
-    if (choice === null) { // if human clicks cancel on prompt
-        console.log("Player cancelled");
-        return;
-    }
+//     if (choice === null) { // if human clicks cancel on prompt
+//         console.log("Player cancelled");
+//         return;
+//     }
 
-    choice = choice.toLowerCase()
+//     choice = choice.toLowerCase()
 
-    if (choice !== "rock" && choice !== "paper" && choice !== "scissors"){
-        console.log("Invalid Input! Try again...");
-        return getHumanChoice();
-    } else {
-        return choice.charAt(0).toUpperCase() + choice.slice(1);
-    }
-}
+//     if (choice !== "rock" && choice !== "paper" && choice !== "scissors"){
+//         console.log("Invalid Input! Try again...");
+//         return getHumanChoice();
+//     } else {
+//         return choice.charAt(0).toUpperCase() + choice.slice(1);
+//     }
+// }
+
+const selectionDiv = document.querySelector(".selection");
+const buttons = selectionDiv.querySelectorAll('button');
+const humanTurn = document.querySelector("p#human-turn");
+const computerTurn = document.querySelector("p#computer-turn");
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log(button.textContent);
+        console.log(`Computer picks: ${getComputerChoice()}`);
+        computerChoice = getComputerChoice();
+        computerTurn.textContent = computerChoice;
+        humanTurn.textContent = button.textContent;
+    });
+});
 
 let humanScore = 0;
 let computerScore = 0;
@@ -48,23 +64,23 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-function playGame(){
-    for (let i = 0; i < 5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
+// function playGame(){
+//     for (let i = 0; i < 5; i++){
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
 
-        if (humanSelection !== undefined && humanSelection !== null){
-            playRound(humanSelection, computerSelection);
-        }
-    }
+//         if (humanSelection !== undefined && humanSelection !== null){
+//             playRound(humanSelection, computerSelection);
+//         }
+//     }
 
-    if (humanScore === computerScore){
-        console.log("It's a tie!");
-    } else if (humanScore > computerScore){
-        console.log("You win!");
-    } else {
-        console.log("You lose!");
-    }
-}
+//     if (humanScore === computerScore){
+//         console.log("It's a tie!");
+//     } else if (humanScore > computerScore){
+//         console.log("You win!");
+//     } else {
+//         console.log("You lose!");
+//     }
+// }
 
-playGame();
+// playGame();
