45e2d89cccc90a7bd9c8126de7fda677b21ab5e8
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-02-21 21:37:45 -0500

    Added restart functionality

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-function isGameOver(humanScore, computerScore){
-    return humanScore === 5 || computerScore === 5;
+function isGameOver(){
+    return humanScore >= 5 || computerScore >= 5;
 }
 
 const selectionDiv = document.querySelector(".selection");
@@ -45,6 +45,8 @@ const computerTurn = document.querySelector("p#computer-turn");
 const humanResult = document.querySelector(".human-score");
 const computerResult = document.querySelector(".computer-score");
 const scoreMsg = document.querySelector(".score-msg");
+const resultInfo = document.querySelector(".result");
+const playAgainBtn = document.querySelector(".restart-btn");
 
 humanResult.textContent = humanScore;
 computerResult.textContent = computerScore;
@@ -60,21 +62,55 @@ function updateScoreInfo(winner, humanChoice, computerChoice){
 }
 
 function handleClick(humanChoice){
-    if (isGameOver()){
+    if (!isGameOver()){
+        computerChoice = getComputerChoice();
+        humanTurn.textContent = humanChoice;
+        computerTurn.textContent = computerChoice;
+
+        playRound(humanChoice, computerChoice);
+
+        humanResult.textContent = humanScore;
+        computerResult.textContent = computerScore;
+        updateScoreInfo(roundWinner, humanChoice, computerChoice);
+    } else {
+        showPlayAgain();
         
-        return;
+        console.log("game over");
+        playAgainBtn.addEventListener("click", () => {
+            humanScore = 0;
+            computerScore = 0;
+            humanResult.textContent = humanScore;
+            computerResult.textContent = computerScore;
+            console.log("play again button");
+            playAgain();
+        });        
     }
-
-    computerChoice = getComputerChoice();
-    humanTurn.textContent = humanChoice;
-    computerTurn.textContent = computerChoice;
-
-    playRound(humanChoice, computerChoice);
-
-    humanResult.textContent = humanScore;
-    computerResult.textContent = computerScore;
-    updateScoreInfo(roundWinner, humanChoice, computerChoice);
 }
+
+function showPlayAgain(){
+    if (playAgainBtn.style.display === "" || playAgainBtn.style.display === "none"){
+        playAgainBtn.style.display = "block";
+    } 
+    console.log(playAgainBtn.style);
+}
+
+function closePlayAgain(){
+    if (playAgainBtn.style.display === "block"){
+        playAgainBtn.style.display = "none";
+    } 
+}
+
+function playAgain(){
+    humanScore = 0;
+    computerScore = 0;
+    closePlayAgain();
+}
+
+// if (!isGameOver()){
+//     rockBtn.addEventListener("click", () => handleClick('Rock'));
+//     paperBtn.addEventListener("click", () => handleClick('Paper'));
+//     scissorsBtn.addEventListener("click", () => handleClick('Scissors'));
+// }
 
 rockBtn.addEventListener("click", () => handleClick('Rock'));
 paperBtn.addEventListener("click", () => handleClick('Paper'));
